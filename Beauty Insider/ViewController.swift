f6fecbd924d60f47f976a1eaa62fc928b9516493
Wilda Cheng <wildacheng5@gmail.com>
2020-05-01 14:18:41 -0400

    Initial Commit

---
+++ Beauty Insider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Beauty Insider
+//
+//  Created by Wilda Cheng on 5/1/20.
+//  Copyright © 2020 Wilda Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
